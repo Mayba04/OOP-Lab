5d6b520481e0590bc23ad6dd6cf8ac8d9be57c78
Pasha Mayba <pashamayba7@gmail.com>
2023-10-03 22:07:07 +0300

    Added Lab8

---
+++ OOP_Lab8/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.RegularExpressions;
+
+class TextChecker
+{
+    public bool ContainsDigits(string inputText)
+    {
+        string pattern = @"\d";
+
+        Regex regex = new Regex(pattern);
+
+        return regex.IsMatch(inputText);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        TextChecker textChecker = new TextChecker();
+        string inputText = "My name is Pavlo and I am 19 y.o. ";
+        Console.WriteLine(inputText);
+        if (textChecker.ContainsDigits(inputText))
+        {
+            Console.WriteLine("This text contains numbers");
+        }
+        else
+        {
+            Console.WriteLine("There is no number in this text");
+        }
+    }
+}
