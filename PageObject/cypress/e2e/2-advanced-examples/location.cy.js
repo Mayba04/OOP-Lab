f09f8b6e5dca466d77445334b5243686d9dc0d5d
Pasha Mayba <pashamayba7@gmail.com>
2023-10-25 11:35:52 +0300

    Test implementation for the sigin page

--- PageObject/cypress/e2e/2-advanced-examples/location.cy.js
+++ PageObject/cypress/e2e/2-advanced-examples/location.cy.js
@@ -1,36 +1,32 @@
+import LoginPage from "./pageObjects/LoginPage.js";
+const loginPage = new LoginPage();
+
 describe("Login", () => {
+ 
   beforeEach(() => {
     cy.visit("http://localhost:3000/dashboard");
   });
 
   it("Should try to log in with all users", () => {
     cy.fixture("login-data.json").then((loginData) => {
-      console.log(loginData); //
       loginData.users.forEach((user) => {
-        cy.get('#email').type(user.email); 
-        cy.get('input[name="password"]').type(user.password, { log: false }); 
-        cy.get('button:contains("Sign In")').click(); 
+        loginPage.typeEmail(user.email)
+                 .typePassword(user.password)
+                 .clickSignIn();
 
         if (user.expectSuccess) {
-          cy.url().should("include", "/dashboard")
-        
+          cy.url().should("include", "/dashboard");
         } else {
-          cy.get('.Toastify__toast--error .Toastify__toast-body').should('be.visible');
-        
-          cy.get('body').then(($body) => {
-            if ($body.find('#email').length) {
-              cy.get('#email').clear()
-            }
-          })
-          
-          cy.get('body').then(($body) => {
-            if ($body.find('input[name="password"]').length) {
-              cy.get('input[name="password"]').clear()
-            }
-          })
+          loginPage.isErrorToastVisible();
         }
-        
-      })
-    })
-  })
-})
+
+        cy.get('body').then(($body) => {
+          if ($body.find('#email').length > 0 && $body.find('input[name="password"]').length > 0) {
+              loginPage.clearFields();
+          }
+      });
+
+      });
+    });
+  });
+});
---
+++ PageObject/cypress/e2e/2-advanced-examples/pageObjects/LoginPage.js
@@ -0,0 +1,33 @@
+export default class LoginPage {
+    get emailInput() { return cy.get('#email'); }
+    get passwordInput() { return cy.get('input[name="password"]'); }
+    get signInButton() { return cy.get('button:contains("Sign In")'); }
+    get errorToast() { return cy.get('.Toastify__toast--error .Toastify__toast-body'); }
+  
+    typeEmail(email) {
+      this.emailInput.type(email);
+      return this; 
+    }
+  
+    typePassword(password) {
+      this.passwordInput.type(password, { log: false });
+      return this;
+    }
+  
+    clickSignIn() {
+      this.signInButton.click();
+      return this;
+    }
+
+    clearFields() {
+      this.emailInput.clear();
+      this.passwordInput.clear();
+      return this;
+    }
+  
+    isErrorToastVisible() {
+      this.errorToast.should('be.visible');
+      return this;
+    }
+  }
+  
